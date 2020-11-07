1aea4f45840b841a1cc5617b948aabcace27865c
Jennifer Morgan <jemorgaine@gmail.com>
2020-11-07 14:06:27 -0600

    switch branches

--- lesson3/examples/src/app/skill-set/skill-set.component.ts
+++ lesson3/examples/src/app/skill-set/skill-set.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimationFrameScheduler } from 'rxjs/internal/scheduler/AnimationFrameScheduler';
 
 @Component({
    selector: 'skill-set',
